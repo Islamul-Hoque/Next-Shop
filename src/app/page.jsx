5f0f81d5deb2b92f9d3e84ac2bb5b0f2f7cccc87
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-11-25 14:30:18 +0600

    feat(categories): introduce animated featured categories section with responsive grid and icon cards

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,8 +7,8 @@ const HomeLayout = () => {
   return (
     <div>
       <Hero/>
-      {/* <FeaturedCategories/>
-      <BestSellingProducts/> */}
+      <FeaturedCategories/>
+      {/* <BestSellingProducts/> */}
     </div>
   );
 };
