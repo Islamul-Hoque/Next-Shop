79c7c5926fbc1a7fa03d445abe1276ea588aa8a3
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-11-28 11:38:12 +0600

    ALl products bug fix

--- src/app/all-products/page.jsx
+++ src/app/all-products/page.jsx
@@ -3,6 +3,7 @@ import ProductFilter from "./ProductFilter";
 const AllProductsPage = async () => {
     const res = await fetch("https://next-shop-api-server.vercel.app/all-products");
     const products = await res.json();
+
     const categories = ["Electronics", "Home & Furniture", "Audio & Music", "Apparel", "Photography", "Accessories"];
 
     return (
